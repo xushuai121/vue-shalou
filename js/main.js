daeefc8bc0ceb50b5fb52dbebce64a6a1ee15bae
xushuaiqueque <xushuai121@163.com>
2018-04-26 17:41:06 +0800

    style

--- js/main.js
+++ js/main.js
@@ -16,26 +16,24 @@ var vm = new Vue({
 				_this.ProjectList = res.data.projectList;
 			})
 		},
-		showProjectInfo:function(e){
-			console.log($(e.target).offset().top);
-			if(typeof item.checked === 'undefined'){
-				if(this.$refs.name.style.width<=(document.documentElement.clientHeight/2)){
-					this.$set(item,'active',true);
-				}else{
-					this.$set(item,'active2',true);
-				}
-			}else{
-				if(this.scrollTop<=(document.documentElement.clientHeight/2)){
-					item.active = true;
-				}else{
-					item.active2 = true;
-				}
-//				item.active = true;
-//				item.active2 = true;
-			}
+		showProjectInfo:function(item,index){
+			console.log(this.$refs.menuItem[index].offsetTop);
 			var _this = this;
 			this.$http.get("http://shalouapi.tuyidesign.com/",{command:"listProjectTask",active:1,project:item.project}).then(function(res){
 				item.listProjectTask = res.data.taskList;
+				if(typeof item.checked === 'undefined'){
+					if(_this.$refs.menuItem[index].offsetTop<=(document.documentElement.clientHeight/2)){
+						_this.$set(item,'active',true);
+					}else{
+						_this.$set(item,'active2',true);
+					}
+				}else{
+					if(_this.scrollTop<=(document.documentElement.clientHeight/2)){
+						item.active = true;
+					}else{
+						item.active2 = true;
+					}
+				}
 			})
 		},
 		hideProjectInfo:function(item){
